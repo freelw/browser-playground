000f766096ce66879aa1793f03f05d6577b1b6b2
freelw <freelw81@qq.com>
2025-07-15 11:33:41 +0800

    deepseek good

---
+++ langchain/1.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+# openai.api_key = os.getenv('DEEPSEEK_API_KEY')
+client = OpenAI(
+    base_url="https://api.deepseek.com/v1",
+    api_key = os.getenv('DEEPSEEK_API_KEY')
+)
+def get_completion(prompt, model="deepseek-chat"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    return response.choices[0].message.content
+
+prompt = "What is the capital of France?"
+response = get_completion(prompt)
+print(response)
+    
